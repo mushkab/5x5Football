8ae528ab96d2d753d408b34d99ce2c0a6bc40387
mush <moshe.kabalo@gmail.com>
2015-10-10 12:51:25 +0300

    ddd

--- football/client/football.js
+++ football/client/football.js
@@ -1,15 +1,23 @@
 
-  Session.setDefault('counter', 0);
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+
+
+
+  Template.gamesList.helpers({
+    games: function () {
+      return Games.find({});
     }
   });
